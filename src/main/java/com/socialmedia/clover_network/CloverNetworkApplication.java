f5ad4ca3b6912fc66f275a93a7bcff470121f2a3
lambmt2k <lambmt.work@gmail.com>
2023-08-01 21:18:20 +0700

    init_code
    init base code

---
+++ src/main/java/com/socialmedia/clover_network/CloverNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.clover_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloverNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloverNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialmedia/clover_network/CloverNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia.clover_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloverNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
