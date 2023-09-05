c3ef7f871d7fc8f574e1d33a3759f9037e94f392
Tùng Lâm <141144593+lambmt2k@users.noreply.github.com>
2023-09-05 10:28:13 +0700

    Create EncryptUtil.java

---
+++ src/main/java/com/socialmedia/clover_network/util/EncryptUtil.java
@@ -0,0 +1,25 @@
+packpackage com.socialmedia.clover_network.util;
+
+public class TripleDesEncryptionUtil {
+  private static final String DESEDE_ENCRYPTION_SCHEME_CIPHER = "DESede/ECB/PKCS5Padding";
+  private static final String UNICODE_FORMAT = "UTF8";
+  private static final String DESEDE_ENCRYPTION_SCHEME_KEY = "DESede";
+  public static String encrypt(String key, String str) throws Exception {
+        Cipher cipher = Cipher.getInstance(DESEDE_ENCRYPTION_SCHEME_CIPHER);
+        SecretKey secretKey1 = new SecretKeySpec(key.getBytes(UNICODE_FORMAT), DESEDE_ENCRYPTION_SCHEME_KEY);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey1);
+        byte[] bytes = cipher.doFinal(str.getBytes(UNICODE_FORMAT));
+        byte[] base64Bytes = Base64.encodeBase64(bytes);
+        return new String(base64Bytes);
+    }
+
+
+    public static String decrypt(String key, String str) throws Exception {
+        byte[] data =Base64.decodeBase64(str);
+        SecretKey secretKey1 = new SecretKeySpec(key.getBytes(UNICODE_FORMAT), DESEDE_ENCRYPTION_SCHEME_KEY);
+        Cipher cipher = Cipher.getInstance(DESEDE_ENCRYPTION_SCHEME_CIPHER);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey1);
+        byte[] decryptedBytes = cipher.doFinal(data);
+        return new String(decryptedBytes, UNICODE_FORMAT);
+    }
+}
