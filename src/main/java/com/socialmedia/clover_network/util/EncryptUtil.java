4cb91a6daa667f9bdefa9e00ee0f883747a10477
Tùng Lâm <141144593+lambmt2k@users.noreply.github.com>
2023-09-05 10:34:36 +0700

    Update EncryptUtil.java

--- src/main/java/com/socialmedia/clover_network/util/EncryptUtil.java
+++ src/main/java/com/socialmedia/clover_network/util/EncryptUtil.java
@@ -4,6 +4,28 @@ public class TripleDesEncryptionUtil {
   private static final String DESEDE_ENCRYPTION_SCHEME_CIPHER = "DESede/ECB/PKCS5Padding";
   private static final String UNICODE_FORMAT = "UTF8";
   private static final String DESEDE_ENCRYPTION_SCHEME_KEY = "DESede";
+
+public TripleDesEncryptionUtil() throws Exception {
+        final MessageDigest md = MessageDigest.getInstance("md5");
+        final byte[] digestOfPassword = md.digest(encryptionSecretKey.getBytes(UNICODE_FORMAT));
+        final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+        for (int j = 0, k = 16; j < 8;) {
+            keyBytes[k++] = keyBytes[j++];
+        }
+        key = new SecretKeySpec(keyBytes, DESEDE_ENCRYPTION_SCHEME_KEY);
+
+
+
+        final byte[] digestOfPassword2 = md.digest(encryptionSecretKeyV2.getBytes(UNICODE_FORMAT));
+        final byte[] keyBytes2 = Arrays.copyOf(digestOfPassword2, 24);
+        for (int j = 0, k = 16; j < 8;) {
+            keyBytes2[k++] = keyBytes2[j++];
+        }
+        key2 = new SecretKeySpec(keyBytes2, DESEDE_ENCRYPTION_SCHEME_KEY);
+
+        iv = new IvParameterSpec(new byte[8]);
+    }
+  
   public static String encrypt(String key, String str) throws Exception {
         Cipher cipher = Cipher.getInstance(DESEDE_ENCRYPTION_SCHEME_CIPHER);
         SecretKey secretKey1 = new SecretKeySpec(key.getBytes(UNICODE_FORMAT), DESEDE_ENCRYPTION_SCHEME_KEY);
