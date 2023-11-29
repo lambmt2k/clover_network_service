b723168c53a21018e838e782fd37a105713df8cd
Tùng Lâm <141144593+lambmt2k@users.noreply.github.com>
2023-11-30 00:21:43 +0700

    Lambmt/logout token (#38)
    * token item info
    
    * logout

--- src/main/java/com/socialmedia/clover_network/dto/res/UserLoginRes.java
+++ src/main/java/com/socialmedia/clover_network/dto/res/UserLoginRes.java
@@ -1,6 +1,7 @@
 package com.socialmedia.clover_network.dto.res;
 
 import com.socialmedia.clover_network.enumuration.UserRole;
+import com.socialmedia.clover_network.enumuration.UserStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,6 @@ public class UserLoginRes {
     private String userId;
     private String tokenId;
     private UserRole userRole;
+    private UserStatus userStatus;
     private LocalDateTime expireTime;
 }
